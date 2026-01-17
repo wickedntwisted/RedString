09855bf01f2eaefb324f20a48cba020325f286ab
digitalyoshixi <digitalyoshixi@proton.me>
2026-01-17 16:50:37 -0500

    feat: remove ui watermark

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css'
 import { DetectiveBoard } from './components/DetectiveBoard'
 
+import { useEffect } from 'react'
+
 function App() {
+  useEffect(() => {
+    // Give time for Tldraw to render watermark, then remove it
+    setTimeout(() => {
+      const watermark = document.querySelector('.tl-watermark_SEE-LICENSE')
+      if (watermark) {
+        watermark.remove()
+      }
+    }, 0)
+  }, [])
+
   return <DetectiveBoard />
 }
 

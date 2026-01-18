07695691faba3973db5f316e30ba670501dd0b48
Daniel <50189988+usernames-are-important@users.noreply.github.com>
2026-01-17 21:03:39 -0500

    Photo upload widget open by default, collapses after first use.

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -8,7 +8,8 @@ interface SearchPanelProps {
 
 export function SearchPanel({ onImageUpload, isSearching }: SearchPanelProps) {
   const [dragActive, setDragActive] = useState(false)
-  const [isExpanded, setIsExpanded] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(true)
+  const [hasUploaded, setHasUploaded] = useState(false)
 
   const handleDrag = (e: React.DragEvent) => {
     e.preventDefault()
@@ -27,12 +28,20 @@ export function SearchPanel({ onImageUpload, isSearching }: SearchPanelProps) {
 
     if (e.dataTransfer.files && e.dataTransfer.files[0]) {
       onImageUpload(e.dataTransfer.files[0])
+      if (!hasUploaded) {
+        setHasUploaded(true)
+        setIsExpanded(false)
+      }
     }
   }
 
   const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       onImageUpload(e.target.files[0])
+      if (!hasUploaded) {
+        setHasUploaded(true)
+        setIsExpanded(false)
+      }
     }
   }
 
